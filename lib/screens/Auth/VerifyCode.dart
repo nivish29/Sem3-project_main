8c908a4eb8251fe9680b6186434505ee395b09e3
nihal <nihal.vish29@gmail.com>
2022-11-17 09:45:14 +0530

    started with Login with phone

---
+++ lib/screens/Auth/VerifyCode.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginWithPhone extends StatefulWidget {
+  const LoginWithPhone({Key? key}) : super(key: key);
+
+  @override
+  State<LoginWithPhone> createState() => _LoginWithPhoneState();
+}
+
+class _LoginWithPhoneState extends State<LoginWithPhone> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
