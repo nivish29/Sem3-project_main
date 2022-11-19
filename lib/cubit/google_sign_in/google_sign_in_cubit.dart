6bdce0634b002f85b6902f64e0eae2547fa97411
nihal <nihal.vish29@gmail.com>
2022-11-19 11:24:19 +0530

    cubit class made

---
+++ lib/cubit/google_sign_in/google_sign_in_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'google_sign_in_state.dart';
+
+class GoogleSignInCubit extends Cubit<GoogleSignInState> {
+  GoogleSignInCubit() : super(GoogleSignInInitial());
+}
